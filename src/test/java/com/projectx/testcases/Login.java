77753c2821ad86009bcfbc7ed43ac2df3bf5fd69
rv-13 <r131194@gmail.com>
2022-12-22 02:09:43 +0530

    Selenium Automation

--- src/test/java/com/projectx/testcases/Login.java
+++ src/test/java/com/projectx/testcases/Login.java
@@ -1,4 +1,48 @@
 package com.projectx.testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class Login {
+
+    @Test
+    public void verifyLoginWithValidCredentials() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://www.tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("amotooricap9@gmail.com");
+        driver.findElement(By.id("input-password")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+        driver.quit();
+
+    }
+    @Test
+    public void verifyLoginWithInValidCredentials() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://www.tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("amotooricap9@gmail.com1");
+        driver.findElement(By.id("input-password")).sendKeys("123451");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        String actualWarning = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+        Assert.assertTrue(true,actualWarning);
+        driver.quit();
+
+    }
 }
