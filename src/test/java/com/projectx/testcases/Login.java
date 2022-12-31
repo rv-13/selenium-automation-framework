483b5a1556a773fb6e68c3a3dd492ba9f7a19f1e
rv-13 <r131194@gmail.com>
2022-12-31 16:55:03 +0530

    Selenium Automation

--- src/test/java/com/projectx/testcases/Login.java
+++ src/test/java/com/projectx/testcases/Login.java
@@ -38,12 +38,31 @@ public class Login {
         driver.get("http://www.tutorialsninja.com/demo/");
         driver.findElement(By.xpath("//span[text()='My Account']")).click();
         driver.findElement(By.linkText("Login")).click();
-        driver.findElement(By.id("input-email")).sendKeys("amotooricap"+generateTimeStamp()+"@gmail.com1");
+        driver.findElement(By.id("input-email")).sendKeys("amotooricap" + generateTimeStamp() + "@gmail.com1");
         driver.findElement(By.id("input-password")).sendKeys("123451");
         driver.findElement(By.xpath("//input[@value='Login']")).click();
 
         String actualWarning = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
         Assert.assertTrue(true, actualWarning);
+        driver.quit();
+
+    }
+
+    @Test
+    public void verifyLoginWithoutCredentials() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://www.tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("input-email")).sendKeys("");
+        driver.findElement(By.id("input-password")).sendKeys("");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        String actualWarningMessage = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+        String expectedWarningMessage = "Warning: No match for E-Mail Address and/or Password.";
+        Assert.assertTrue(actualWarningMessage.contains(expectedWarningMessage), "Warning: No match for E-Mail Address and/or Password.");
         driver.quit();
 
     }
