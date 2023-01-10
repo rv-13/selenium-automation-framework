f202a0e252433e1923f657a552c1a8b2f8ad7fdb
rv-13 <r131194@gmail.com>
2023-01-11 02:46:10 +0530

    Selenium Automation

--- src/test/java/com/projectx/testcases/Register.java
+++ src/test/java/com/projectx/testcases/Register.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -13,7 +14,12 @@ public class Register {
 
     WebDriver driver;
 
-    @Test
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test(priority = 2)
     public void verifyRegisteringWithMandatoryFields() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -33,6 +39,56 @@ public class Register {
 
         String actualsuccessMessage = driver.findElement(By.xpath("//div[@id='content']/h1")).getText();
         Assert.assertEquals(actualsuccessMessage, "Your Account Has Been Created!");
+        driver.quit();
+
+
+    }
+
+    @Test(priority = 3)
+    public void verifyRegisteringWithMandatoryWithRadioOptionFields() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://www.tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("Rv");
+        driver.findElement(By.id("input-lastname")).sendKeys("S");
+        driver.findElement(By.id("input-email")).sendKeys(Utilities.generateEmailWithTimeStamp());
+        driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+        driver.findElement(By.id("input-password")).sendKeys("12345");
+        driver.findElement(By.id("input-confirm")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@name='newsletter']")).click();
+        driver.findElement(By.name("agree")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+        String actualsuccessMessage = driver.findElement(By.xpath("//div[@id='content']/h1")).getText();
+        Assert.assertEquals(actualsuccessMessage, "Your Account Has Been Created!");
+        driver.quit();
+
+
+    }
+
+    @Test(priority = 1)
+    public void verifyRegisteringWithMandatoryWithoutPolicy() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://www.tutorialsninja.com/demo/");
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("Rv");
+        driver.findElement(By.id("input-lastname")).sendKeys("S");
+        driver.findElement(By.id("input-email")).sendKeys(Utilities.generateEmailWithTimeStamp());
+        driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+        driver.findElement(By.id("input-password")).sendKeys("12345");
+        driver.findElement(By.id("input-confirm")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+        String actualErrorMessage = driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+        Assert.assertEquals(actualErrorMessage, "Warning: You must agree to the Privacy Policy!");
         driver.quit();
 
 
