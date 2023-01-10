70229d9870c613c5e9e16fb92c6a367ab76e04b7
rv-13 <r131194@gmail.com>
2023-01-11 03:44:49 +0530

    Selenium Automation

--- src/test/java/com/projectx/testcases/Search.java
+++ src/test/java/com/projectx/testcases/Search.java
@@ -3,6 +3,7 @@ package com.projectx.testcases;
 import com.projectx.base.BaseClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,6 +26,25 @@ public class Search extends BaseClass {
     public void verifySearchWithValidProduct() {
         driver.findElement(By.name("search")).sendKeys("HP");
         driver.findElement(By.xpath("//div[@id='search']/descendant::button")).click();
+        Assert.assertTrue(driver.findElement(By.linkText("HP LP3065")).isDisplayed());
+
+    }
+
+    @Test
+    public void verifySearchWithInValidProduct() {
+        driver.findElement(By.name("search")).sendKeys("avxj");
+        driver.findElement(By.xpath("//div[@id='search']/descendant::button")).click();
+        String noProductFoundMessage = driver.findElement(By.xpath("//input[@id='button-search']/following-sibling::p")).getText();
+        Assert.assertTrue(noProductFoundMessage.contains("There is no product that matches the search criteria."));
+
+    }
+
+    @Test
+    public void verifySearchWithNoProduct() {
+        driver.findElement(By.name("search")).sendKeys("");
+        driver.findElement(By.xpath("//div[@id='search']/descendant::button")).click();
+        String noProductFoundMessage = driver.findElement(By.xpath("//input[@id='button-search']/following-sibling::p")).getText();
+        Assert.assertTrue(noProductFoundMessage.contains("There is no product that matches the search criteria."));
 
     }
 }
